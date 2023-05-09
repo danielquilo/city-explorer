e8f7befa673f63cdd9132fc875922924fec68990
danielquilo <danielquilo1012@yahoo.com>
2023-05-09 17:57:37 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import axios from "axios";
 import { useState } from "react";
-
+import Local from './Geolocation';
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
