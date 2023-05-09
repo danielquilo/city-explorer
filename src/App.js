2a7e232856a80c4c8de60600f6d1d039185e0e8f
danielquilo <danielquilo1012@yahoo.com>
2023-05-09 13:50:36 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [pokemon, setPokemon] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  async function getPokemon() {
+    try {
+      const API = "https://pokeapi.co/api/v2/pokemon/" + searchQuery.toLowerCase();
+      const res = await axios.get(API);
+      setPokemon(res.data.sprites.front_default);
+    } catch (error) {
+      console.log(error);
+      // show a 404 image when there is an error... NICE
+      setPokemon("https://aioseo.com/wp-content/uploads/2021/04/how-to-find-and-fix-404-errors-in-wordpress.png.webp");
+    }
+  }
+
+  function handleChange(event) {
+    setSearchQuery(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>API Demo</h1>
+      <input onChange={handleChange} placeholder="What Pokemon?" />
+      <button onClick={getPokemon}>Show me the Pokemon!</button>
+      {pokemon && <img src={pokemon} alt="Pokemon" />}
     </div>
   );
 }
