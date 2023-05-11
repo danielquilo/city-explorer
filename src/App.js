b7351a9157c6bee00e6197d5b92cd9f39740b325
danielquilo <danielquilo1012@yahoo.com>
2023-05-11 10:34:42 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import "./App.css";
 import axios from "axios";
-import { useState } from "react";
-
+import hotBg from "./assets/hot.jpg";
+import coldBg from "./assets/cold.jpg";
+import Descriptions from "./components/Descriptions";
+import { useEffect, useState } from "react";
+import { getFormattedWeatherData } from "./weatherService";
 
 
 function App() {
+  const [city, setCity] = useState("Paris");
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState("metric");
+  const [bg, setBg] = useState(hotBg);
   const [searchQuery, setSearchQuery] = useState("");
   const [location, setLocation] = useState({});
-  const [map,setMap] =useState ("");
-  const[apiError, setApiError] = useState("");
 
   function handleChange(event) {
     setSearchQuery(event.target.value);
@@ -19,33 +23,78 @@ function App() {
       // https://eu1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json
       const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
       const res = await axios.get(API);
-      const newLocation = res.data[0];
-      setLocation(newLocation);
-      getMap(newLocation);
-      setApiError("");
+      setLocation(res.data[0]);
     } catch (error) {
       console.log(error);
-      setApiError(error.message);
-      setLocation({});
-      setMap("");
     }
   }
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getFormattedWeatherData(city, units);
+      setWeather(data);
 
-  async function getMap(newLocation) {
-    const API =  `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${newLocation.lat},${newLocation.lon}&zoom=11`;
-    setMap(API);
+      // dynamic bg
+      const threshold = units === "metric" ? 20 : 60;
+      if (data.temp <= threshold) setBg(coldBg);
+      else setBg(hotBg);
+    };
 
-  }
+    fetchWeatherData();
+  }, [units, city]);
 
+  const handleUnitsClick = (e) => {
+    const button = e.currentTarget;
+    const currentUnit = button.innerText.slice(1);
+
+    const isCelsius = currentUnit === "C";
+    button.innerText = isCelsius ? "°F" : "°C";
+    setUnits(isCelsius ? "metric" : "imperial");
+  };
+
+  const enterKeyPressed = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value);
+      e.currentTarget.blur();
+    }
+  };
 
   return (
-  <div className="App">
-      <h1>City Explorer Demo</h1>
-      <input onChange={handleChange} placeholder="Place name" />
-      <button onClick={getLocation}>Explore</button>
-      <p>{apiError}</p>
-      <h2>{location.display_name}</h2>
-      {map && <img src={map} alt="map" />}
+    <div className="app" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="overlay">
+        {weather && (
+          <div className="container">
+            <div className="section section__inputs">
+              <input
+              onChange={handleChange}
+                onKeyDown={enterKeyPressed}
+                type="text"
+                name="city"
+                placeholder="Enter City..."
+              />
+              
+              <button onClick={(e) => handleUnitsClick(e)}>°F</button>
+                 
+                  </div>
+
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>{`${weather.name}, ${weather.country}`}</h3>
+                <img src={weather.iconURL} alt="weatherIcon" />
+                <h3>{weather.description}</h3>
+              </div>
+              <div className="temperature">
+                <h1>{`${weather.temp.toFixed()} °${
+                  units === "metric" ? "C" : "F"
+                }`}</h1>
+              </div>
+            </div>
+            
+
+            {/* bottom description */}
+            <Descriptions weather={weather} units={units} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
